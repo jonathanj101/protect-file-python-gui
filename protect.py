8d549840f0992a6c7a763cee551bc4c5a3197f61
jonathanj101 <jonatjimen@gmail.com>
2021-11-08 14:56:48 -0500

    gui initialized, selecting file working, need authentication

--- protect.py
+++ protect.py
@@ -1,16 +1,45 @@
+import subprocess, os, platform
 from tkinter import *
+from tkinter import filedialog
 
 ##creating the main window and storing the window object in 'win'
 win=Tk()
 # setting the size of the window
 win.geometry('400x200') 
 
-Label(win,text="Name").grid(row=0)
-Label(win,text="Email").grid(row=1)
+def protect_path(user,password,filepath):
+    user_input = input("username: ")
+    password_input = input("password: ")
 
-ent1 = Entry(win)
-ent2 = Entry(win)
-ent1.grid(row=0,column=1)
-ent2.grid(row=1,column=1)
+    if user == user_input and password==password_input:
+        openfile = open(filepath,'r')
+        print(openfile)
+        print(platform.system())
+        if platform.system() == "Linux":
+            subprocess.call(('xdg-open',filepath))
+    else:
+        print("incorrect")
+
+def get_filepath():
+    filepath = filedialog.askopenfilename(initialdir="/media/jonathan/Storage/documents/Security/")
+    print(filepath)
+    protect_path("jon","jon", filepath)
+
+def get_value():
+    try:
+        value = ent1.get()
+        print(value)
+    except ValueError:
+        pass
+
+label_name = Label(win,text='Name').grid(row=0)
+label_email = Label(win,text="Email").grid(row=1)
+
+btn = Button(text="Get Value", command=get_value).grid(column=2,row=2)
+btn = Button(text="filepath", command=get_filepath).grid(column=1,row=3)
+
+
+ent1 = Entry(win).grid(row=0,column=1)
+ent2 = Entry(win).grid(row=1,column=1)
 
 mainloop()
