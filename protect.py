6f2d6166eeaf82a672b0ae53303564de8439f3df
jonathanj101 <jonatjimen@gmail.com>
2021-11-09 21:19:39 -0500

    need some positioning, thinking db etc

--- protect.py
+++ protect.py
@@ -7,39 +7,40 @@ win=Tk()
 # setting the size of the window
 win.geometry('400x200') 
 
+def check_system_platform(filepath):
+    if platform.system() == "Darwin":
+        subprocess.call(('open',filepath))
+    elif platform.system == "Windows":
+        os.startfile(filepath)
+    else:
+        subprocess.call(('xdg-open',filepath))
+
 def protect_path(user,password,filepath):
     user_input = input("username: ")
     password_input = input("password: ")
 
     if user == user_input and password==password_input:
-        openfile = open(filepath,'r')
-        print(openfile)
-        print(platform.system())
-        if platform.system() == "Linux":
-            subprocess.call(('xdg-open',filepath))
+        check_system_platform(filepath)
     else:
-        print("incorrect")
+        print("incorrect password")
 
 def get_filepath():
     filepath = filedialog.askopenfilename(initialdir="/media/jonathan/Storage/documents/Security/")
     print(filepath)
     protect_path("jon","jon", filepath)
 
-def get_value():
-    try:
-        value = ent1.get()
-        print(value)
-    except ValueError:
-        pass
+# def get_value():
+#     try:
+#         value = ent1.get()
+#         print(value)
+#     except ValueError:
+#         pass
+# filepath label
+# filepath_label = Label(win,text='Name').grid(row=0)
+# Display an input to add text..
+# filepath_entry = Entry(win).grid(row=0,column=1)
 
-label_name = Label(win,text='Name').grid(row=0)
-label_email = Label(win,text="Email").grid(row=1)
-
-btn = Button(text="Get Value", command=get_value).grid(column=2,row=2)
 btn = Button(text="filepath", command=get_filepath).grid(column=1,row=3)
 
-
-ent1 = Entry(win).grid(row=0,column=1)
-ent2 = Entry(win).grid(row=1,column=1)
 
 mainloop()
